2dd92b83583c523b987efc728a1aded8c67b7a61
Karthik Dintakurthi <d.karthiknaidu@gmail.com>
2023-09-09 20:40:25 -0400

    f

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const fs = require('fs');
 const cors = require('cors'); // Import the 'cors' middleware
 const bodyParser = require('body-parser');
 
